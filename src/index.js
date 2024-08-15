d472154b3c3f147868f1a8cce1a640db45cef5da
Rashid Ameer <r.ameer.offical@gmail.com>
2024-08-15 20:58:17 +0500

    Added class for error and response. Added async handler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 // dot env configuration
 dotenv.config({ path: "./env" });
 
+// PORT
+const PORT = process.env.PORT || 5000;
+
 // connecting to database
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+
+    app.on("error", (error) => {
+      console.log("Error in running server", error);
+    });
+  })
+  .catch((error) => {
+    console.log("Error in connecting to database", error);
+    process.exit(1);
+  });
---
+++ src/utils/async-handler.js
@@ -0,0 +1,12 @@
+const asyncHanlder = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res
+      .status(error.statusCode || 500)
+      .json({
+        success: false,
+        message: error.message || "Internal Server Error",
+      });
+  }
+};
